47be11c373d448f99b79a37b55b73642f18c1bbb
Abhishek S <67963799+iamA333@users.noreply.github.com>
2022-06-12 12:08:16 +0530

    Added UI files

---
+++ UI/style.css
@@ -0,0 +1,47 @@
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;900&display=swap');
+
+body {
+    background-image: url("background.png");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    font-family: 'Montserrat', sans-serif;
+  }
+  
+  h1 {
+    text-align: center;
+    margin-top: 30px;
+    font-weight: 900;
+    font-size: 72x;
+    color: #283177;
+  }
+  
+  .form-div {
+    width: 600px;
+    margin: 5% auto;
+    text-align: center;
+    background-color: #0aa1dd;
+    padding: 100px;
+    border-radius: 20px;
+    align-items: center;
+  }
+.option{
+    width: 35%;
+    align-items: center;
+    margin-top: 7%;
+}  
+.button{
+    background-color: #283177;
+    width: 50%;
+    margin: 7%;
+}
+.audio-output{
+width: 100%;
+}
+  #form-main-text {
+    font-size: large;
+    font-weight: bold;
+  }
+
+
+  
